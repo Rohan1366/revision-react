0eb18e698c9e044ab7f9dafbcdec1e5314a1097b
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-08-25 18:35:19 +0530

    aded

--- props/src/App.js
+++ props/src/App.js
@@ -8,13 +8,15 @@ function App(props) {
     < >
     <h1>Hello World</h1>
     <div className="App">
-      <Header name="rohan" age="20" city="vita" />
+     {/* <Header name="rohan" age="20" city="vita" />
       
       <Card img="https://th.bing.com/th/id/OIP.pP0DWi9pWVj3e2Rk_hzdKAHaLG?pid=ImgDet&w=199&h=298&c=7&dpr=1.3" name="Rohan" gender="male"/>
       <Card img="https://th.bing.com/th/id/OIP.n9eY1B5cduYR8tTEtVgD8QHaLi?pid=ImgDet&w=199&h=310&c=7&dpr=1.3" name="Ritesh" gender="male"/>
       <Card img="https://th.bing.com/th/id/OIP.7-PVtrGRSm80FAdJHjSRPgHaF7?pid=ImgDet&rs=1" name="Start" gender="Female"/>
       <Card img="https://th.bing.com/th/id/OIP.22EylDsZ2jCkidoAe6yxNAHaHa?pid=ImgDet&w=199&h=199&c=7&dpr=1.3" name="Zoya" gender="Female"/>
-    </div>
+  */}
+ 
+      </div>
     </>
 
   );
