413e7bea3a59f2b5982633029af1d32ec1988721
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-09-08 19:38:55 +0530

    added

--- hooks/src/Routing/Products/ProductDetails.js
+++ hooks/src/Routing/Products/ProductDetails.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
 const ProductDetails = () => {
+   const pathParams= useParams()
+   console.log(pathParams)
   return (
     <div>ProductDetails</div>
   )
