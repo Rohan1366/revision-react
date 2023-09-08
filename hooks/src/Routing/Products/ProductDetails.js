b9113ad5911f124cacc8569fac67a2bbe5bb1c62
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-09-08 20:06:24 +0530

    added

--- hooks/src/Routing/Products/ProductDetails.js
+++ hooks/src/Routing/Products/ProductDetails.js
@@ -9,12 +9,24 @@ const ProductDetails = () => {
        .then((res)=>res.json())
        .then((data)=>{
         setState(data)
+        console.log(data)
        })
    },[])
   return (
-    <div>
+    
+       
+           
+               <div style={{alignItem:"center",textAlign:"center",width:"400px",margin:"30px auto ",boxShadow:"0 0 10px black",borderRadius:"10px"}}>
+                
+                    <h1 style={{paddingTop:"10px"}}>{state.category} Details</h1>
+                    <img src={state.image} style={{height:"300px",width:"300px"}} />
+                    <h3>Price :{state.price}</h3>
+                    <p>{state.description}</p>
+               </div>
         
-    </div>
+
+
+   
   )
 }
 
