87f6d0513f57e40a779ddf172b8cb23888b50d1d
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-09-08 19:42:59 +0530

    added

--- hooks/src/Routing/Products/ProductDetails.js
+++ hooks/src/Routing/Products/ProductDetails.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 const ProductDetails = () => {
    const pathParams= useParams()
-   console.log(pathParams)
+   
+   const [state,setState]=useState({})
+   useEffect(()=>{
+       fetch("https://fakestoreapi.com/products/1")
+       .then((res)=>res.json())
+       .then((data)=>{
+        setState(data)
+       })
+   },[])
   return (
     <div>ProductDetails</div>
   )
